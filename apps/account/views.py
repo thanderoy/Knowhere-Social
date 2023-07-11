2e6bfd45ee22f2b961fe67119f26be417de18faa
Thande Roy <39765073+thanderoy@users.noreply.github.com>
2023-07-11 21:23:14 +0300

    Add Authentication Workflows (#2)
    * Add logout worflows
    
    * Add authentication framework/workflows

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,39 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, 
+                username=data.get("username"), 
+                password=data.get("password")
+            )
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Authenticated successfully.")
+                else:
+                    return HttpResponse("User account not active.")
+            else:
+                return HttpResponse("Invalid credentials.")
+        else:
+            form = LoginForm()
+
+        context = {"form": form}
+
+        return render(request, "account/login.html", context)
+
+
+@login_required
+def dashboard(request):
+    context = {"section": "dashboard"}
+    return render(request, "account/dashboard.html", context)
