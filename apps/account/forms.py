999de13e413e2071b0389898238783c3c6e460bb
Roy Thande <roy.thande@gmail.com>
2023-07-12 22:49:07 +0300

    Add User Profile and Registration

--- apps/account/forms.py
+++ apps/account/forms.py
@@ -1,6 +1,39 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.Form):
+    password = forms.CharField(
+        label="Password", widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label="Confirm Password", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "email"]
+
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data.get("password") != data.get("password2"):
+            raise forms.ValidationError("Passwords don't match")
+        return data.get("password2")
+
+
+class UserEditForm(forms.Form):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["date_of_birth", "photo"]
--- apps/account/views.py
+++ apps/account/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 def user_login(request):
@@ -12,8 +13,8 @@ def user_login(request):
         if form.is_valid():
             data = form.cleaned_data
             user = authenticate(
-                request, 
-                username=data.get("username"), 
+                request,
+                username=data.get("username"),
                 password=data.get("password")
             )
 
@@ -31,6 +32,54 @@ def user_login(request):
         context = {"form": form}
 
         return render(request, "account/login.html", context)
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data.get("password"))
+            user.save()
+            Profile.objects.create(user=user)
+
+            context = {
+                "user": user
+            }
+            return render(request, "account/register_done.html", context)
+
+    elif request.method == "GET":
+        form = UserRegistrationForm()
+
+    return render(request, "account/register.html", context)
+
+
+@login_required
+def edit(request):
+    if request.method == "POST":
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    elif request.method == "GET":
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form,
+    }
+    return render(request, "account/edit.html", context)
 
 
 @login_required
