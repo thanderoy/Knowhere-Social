eacab08a561b8d2921926128bafffb83abfccd33
Roy Thande <roy.thande@gmail.com>
2023-07-27 23:09:43 +0300

    Allow for Image Downloads

--- apps/images/forms.py
+++ apps/images/forms.py
@@ -1,9 +1,12 @@
+import requests
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 from .models import Image
 
 
-class ImageCreateForm(forms.ModelChoiceField):
+class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ["title", "url", "description"]
@@ -13,10 +16,29 @@ class ImageCreateForm(forms.ModelChoiceField):
         }
 
         def clean_url(self):
-            url = self.cleaned_data['url']
+            url = self.cleaned_data.get("url")
             valid_extensions = ['jpg', 'jpeg', 'png']
             extension = url.rsplit('.', 1)[1].lower()
             if extension not in valid_extensions:
                 raise forms.ValidationError(
                     'The given URL does not match valid image extensions.')
             return url
+
+        def save(self, force_insert=False, force_update=False, commit=True):
+            image = super().save(commit=False)
+            image_url = self.cleaned_data.get("url")
+            name = slugify(image.title)
+            extension = image_url.rsplit('.', 1)[1].lower()
+            image_name = f"{name}.{extension}"
+
+            # Download image from given URL
+            response = requests.get(image_url)
+            image.image.save(
+                image_name,
+                ContentFile(response.content),
+                save=False
+            )
+
+            if commit:
+                image.save()
+            return image
--- apps/images/views.py
+++ apps/images/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == "GET":
+        form = ImageCreateForm(data=request.GET)
+        
+    elif request.method == "POST":
+        form = ImageCreateForm(data=request.POST)
+        
+        
+        if form.is_valid():
+            data = form.cleaned_data
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+            
+            messages.success(request, "Image added successfully.")
+            
+            return redirect(new_image.get_absolute_url())
+        
+    context = {
+        'section': 'images',
+        'form': 'form'
+    }
+    return render(request, 'images/image/create.html', context)
